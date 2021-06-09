d8de008add204f273d0ff2d46cb04441ac4a754f
Dustin Fox <djf149@hotmail.com>
2021-06-09 17:36:34 -0400

    Add project files.

---
+++ Playing_around_with_Types/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Playing_around_with_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Aplle";
+            var b = "Bus";
+            var c = "Cat";
+
+            int aB = 1;
+            int cD = 2;
+            double eF = 123456789;
+
+            var anything = Console.ReadLine();
+
+
+
+            Console.WriteLine(anything);
+        }
+    }
+}
